2c12aa9617998d9be11d4fa06cea466e093bf34d
DAMIAN MAURICIO DIAZ TOLEDO <damian.diaz04@inacapmail.cl>
2018-04-13 14:48:47 -0300

    QR
    Co-Authored-By: Michel Valenzuela <mixelreyes@hotmail.com>

---
+++ Qr_v2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Qr_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtQR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.imagenQR = new System.Windows.Forms.PictureBox();
+            this.imagenAbrir = new System.Windows.Forms.PictureBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imagenQR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagenAbrir)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtQR
+            // 
+            this.txtQR.Location = new System.Drawing.Point(289, 38);
+            this.txtQR.Name = "txtQR";
+            this.txtQR.Size = new System.Drawing.Size(216, 20);
+            this.txtQR.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(289, 74);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(216, 23);
+            this.btnGenerar.TabIndex = 3;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Enabled = false;
+            this.btnGuardar.Location = new System.Drawing.Point(289, 112);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(216, 23);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // imagenQR
+            // 
+            this.imagenQR.Location = new System.Drawing.Point(25, 22);
+            this.imagenQR.Name = "imagenQR";
+            this.imagenQR.Size = new System.Drawing.Size(200, 200);
+            this.imagenQR.TabIndex = 5;
+            this.imagenQR.TabStop = false;
+            // 
+            // imagenAbrir
+            // 
+            this.imagenAbrir.Location = new System.Drawing.Point(563, 22);
+            this.imagenAbrir.Name = "imagenAbrir";
+            this.imagenAbrir.Size = new System.Drawing.Size(200, 200);
+            this.imagenAbrir.TabIndex = 6;
+            this.imagenAbrir.TabStop = false;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(289, 153);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(216, 23);
+            this.btnAbrir.TabIndex = 7;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(289, 192);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(216, 20);
+            this.txtResultado.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 246);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.imagenAbrir);
+            this.Controls.Add(this.imagenQR);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQR);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generador QR";
+            ((System.ComponentModel.ISupportInitialize)(this.imagenQR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagenAbrir)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtQR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.PictureBox imagenQR;
+        private System.Windows.Forms.PictureBox imagenAbrir;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ Qr_v2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace Qr_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = new QrCode();
+            qrEncoder.TryEncode(txtQR.Text, out qrCode);
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+            MemoryStream ms = new MemoryStream();
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+            var imageTemporal = new Bitmap(ms);
+            var image = new Bitmap(imageTemporal, new Size(new Point(200, 200)));
+           
+            imagenQR.Image = image;
+            btnGuardar.Enabled = true;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+           
+            SaveFileDialog sfd = new SaveFileDialog()
+            {
+                Filter = "Image PNG(*.png)|*.png",
+                InitialDirectory = @"C:\Users\Damian\Desktop"
+            };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                imagenQR.Image.Save(sfd.FileName);
+            }
+
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Filter = "Image PNG(*.png)|*.png",
+                InitialDirectory = @"C:\Users\Gepar\Desktop\Codigos barra"
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                imagenAbrir.Image = Image.FromFile(ofd.FileName);
+                BarcodeReader br = new BarcodeReader();
+                string texto = br.Decode((Bitmap)imagenAbrir.Image).ToString();
+                txtResultado.Text = texto;
+            }
+        }
+    }
+}
